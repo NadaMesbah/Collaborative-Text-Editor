092275980ef2461428169134280bb5b788a1cf55
nada <nada.mesbah@usmba.ac.ma>
2023-02-25 19:23:51 +0100

    Documents Controller

---
+++ Controllers/DocumentsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealTimeCollaborativeApp.Data;
+using RealTimeCollaborativeApp.Models;
+
+namespace RealTimeCollaborativeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DocumentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Documents
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Document>>> GetDocuments()
+        {
+          if (_context.Documents == null)
+          {
+              return NotFound();
+          }
+            return await _context.Documents.ToListAsync();
+        }
+
+        // GET: api/Documents/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Document>> GetDocument(int id)
+        {
+          if (_context.Documents == null)
+          {
+              return NotFound();
+          }
+            var document = await _context.Documents.FindAsync(id);
+
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            return document;
+        }
+
+        // PUT: api/Documents/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDocument(int id, Document document)
+        {
+            if (id != document.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(document).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DocumentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Documents
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Document>> PostDocument(Document document)
+        {
+          if (_context.Documents == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Documents'  is null.");
+          }
+            _context.Documents.Add(document);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDocument", new { id = document.Id }, document);
+        }
+
+        // DELETE: api/Documents/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDocument(int id)
+        {
+            if (_context.Documents == null)
+            {
+                return NotFound();
+            }
+            var document = await _context.Documents.FindAsync(id);
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            _context.Documents.Remove(document);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DocumentExists(int id)
+        {
+            return (_context.Documents?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
