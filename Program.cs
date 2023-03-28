0d5313b0ed506d5ae9e923f11002b05364bfd340
nada <nada.mesbah@usmba.ac.ma>
2023-03-28 15:36:01 +0000

    updates

--- Program.cs
+++ Program.cs
@@ -43,6 +43,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 app.MapHub<TextEditorHub>("/hubs/texteditor");
-app.MapHub<CreateHub>("/hubs/create");
 
 app.Run();
